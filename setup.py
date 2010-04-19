58df9bdb36f96a33a4a3fb73ceff355644a1e9af
maxpeterson <max@incuna.com>
2010-04-19 13:54:56 +0000

    Added incunafein app/area

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = "incunafein",
+    packages = ["incunafein", ],
+    version = "0.1",
+    description = "Provides enhancements to FeinCMS.",
+    author = "Incuna Ltd",
+    author_email = "admin@incuna.com",
+    url = "http://incuna.com/",
+    # download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
+    # long_description = """"""
+)
