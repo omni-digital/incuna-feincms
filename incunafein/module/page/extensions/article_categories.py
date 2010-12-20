8baee4c553c1b19fcb360c2ca8049f844cd0a642
maxpeterson <max@incuna.com>
2010-12-20 14:32:53 +0000

    Adding article_categories page extension

--- incunafein/module/page/extensions/article_categories.py
+++ incunafein/module/page/extensions/article_categories.py
@@ -8,26 +8,27 @@ from django.utils.translation import ugettext_lazy as _
 from feincms.module.page.models import Page
 
 def register(cls, admin_cls):
-    cls.add_to_class('articles', models.ManyToManyField('articles.Article', blank=True,
+    cls.add_to_class('article_categories', models.ManyToManyField('articles.Category', blank=True,
         #related_name='%(app_label)s_%(class)s_related',
-        null=True, help_text=_('Select articles that should be listed as related content.')))
+        null=True, help_text=_('Select categories that should be listed as related content.')))
 
     try:
-        admin_cls.filter_horizontal.append('articles')
+        admin_cls.filter_horizontal.append('article_categories')
     except AttributeError:
-        admin_cls.filter_horizontal = ['articles']
+        admin_cls.filter_horizontal = ['article_categories']
 
     title = _('Articles')
     found = False
     for sets in admin_cls.fieldsets:
         if sets[0] == title:
-            sets[1]['fields'] = list(sets[1]['fields']) + ['articles',]
+            sets[1]['fields'] = list(sets[1]['fields']) + ['article_categories',]
             found = True
             break
 
     if not found:
         admin_cls.fieldsets.append((title, {
-            'fields': ['articles',],
+            'fields': ['article_categories',],
             'classes': ('collapse',),
         }))
+
 
