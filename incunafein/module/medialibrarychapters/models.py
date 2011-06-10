b93f1f6eb0f4f54b3f0ebd742e336810a236a004
maxpeterson <max@incuna.com>
2011-06-10 10:11:20 +0000

    Removed chapters from non video or audio types; renamed to medialibrarychapters.MediaFileChapter

--- incunafein/module/medialibrarychapters/models.py
+++ incunafein/module/medialibrarychapters/models.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.db import models
-from feincms.module.medialibrary.models import MediaFile
 
-VIDEO_TYPE = 'video'
+CHAPTER_TYPES = ('video', 'audio')
 
-class VideoChapter(models.Model):
-    video = models.ForeignKey('medialibrary.MediaFile', limit_choices_to = {'type': VIDEO_TYPE})
+class MediaFileChapter(models.Model):
+    media_file = models.ForeignKey('medialibrary.MediaFile', limit_choices_to = {'type__in': CHAPTER_TYPES})
     title = models.CharField(max_length=255)
     timecode = models.TimeField(help_text='hh:mm:ss')
     preview = models.ImageField(upload_to='medialibrary/section/', null=True, blank=True)
@@ -20,10 +18,3 @@ class VideoChapter(models.Model):
     @property
     def seconds(self):
         return self.timecode.hour*3600+self.timecode.minute*60+self.timecode.second
-
-class VideoChapterAdmin(admin.ModelAdmin):
-    list_display      = ['video', 'title', 'timecode']
-    list_filter       = ['video']
-    search_fields     = ['title']
-
-
--- incunafein/module/videochapters/extensions/chapters.py
+++
@@ -1,9 +0,0 @@
-from incunafein.module.videochapters.models import VideoChapter
-def register(cls, admin_cls):
-    if admin_cls:
-        from django.contrib import admin
-        class VideoChaptersInline(admin.TabularInline):
-            model = VideoChapter
-            extra = 3
-
-        admin_cls.inlines = list(admin_cls.inlines) + [VideoChaptersInline, ]
