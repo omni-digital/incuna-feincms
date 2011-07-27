19252f84749ec8654bfcf4a879999439ac1ee7ba
George Hickman <george@incuna.com>
2011-07-27 15:19:58 +0100

    Check the admin class has show_on_top variable which isn't necessarily in FeinCMS
    At some point FeinCMS removed the show_on_top variable so make sure it's available before we use it.

--- incunafein/module/page/extensions/show_title.py
+++ incunafein/module/page/extensions/show_title.py
@@ -1,11 +1,13 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 """
 Add a show_title flag to the page
 """
 
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
 def register(cls, admin_cls):
     cls.add_to_class('show_title', models.BooleanField(_('show title'), default=True,))
 
-    admin_cls.show_on_top += ['show_title']
+    if hasattr(admin_cls, 'show_on_top'):
+        admin_cls.show_on_top += ['show_title']
+
