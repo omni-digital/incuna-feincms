46bcad1e20e57f66498e7a70b8f3be929115bde6
George Hickman <george@incuna.com>
2011-10-12 15:26:37 +0100

    Return parent date if there isn't one on the current object
    Look for a prepared date in the ancestors of the current object and use
    that if it exists

--- incunafein/module/page/extensions/prepared_date.py
+++ incunafein/module/page/extensions/prepared_date.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-def get_prepared_date(cls):
-    return cls.prepared_date or cls.parent.prepared_date
-
 def register(cls, admin_cls):
-    cls.add_to_class('prepared_date', models.TextField('Date of Preparation', blank=True, null=True))
-    cls.add_to_class('get_prepared_date', get_prepared_date)
+    cls.add_to_class('_prepared_date', models.TextField('Date of Preparation', blank=True, null=True))
+
+    def getter():
+        if not cls._prepared_date:
+            try:
+                return cls.get_ancestors(ascending=True).filter(_prepared_date__isnull=False)[0]._prepared_date
+            except IndexError:
+                return None
+        return cls._prepared_date
+
+    def setter(value):
+        cls._prepared_date = value
+
+    cls.prepared_date = property(getter, setter)
+
+    if admin_cls and admin_cls.fieldsets:
+        admin_cls.fieldsets[2][1]['fields'].append('_prepared_date')
 
