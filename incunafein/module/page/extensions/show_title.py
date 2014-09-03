11c8b82d9dffc2c3d75afa4b118bc9c71b7847a3
Max Peterson <max@incuna.com>
2014-09-03 15:53:42 +0100

    Update show_title extension.

--- incunafein/module/page/extensions/show_title.py
+++ incunafein/module/page/extensions/show_title.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from feincms import extensions
 
 """
 Add a show_title flag to the page
 """
 
-def register(cls, admin_cls):
-    cls.add_to_class('show_title', models.BooleanField(_('show title'), default=True,))
 
-    if hasattr(admin_cls, 'show_on_top'):
-        admin_cls.show_on_top += ['show_title']
+class Extension(extensions.Extension):
+    def handle_model(self):
+        self.model.add_to_class(
+            'show_title',
+            models.BooleanField(_('show title'), default=True),
+        )
 
+    def handle_modeladmin(self, modeladmin):
+        modeladmin.add_extension_options(_('Show title'), {
+            'fields': ('show_title',),
+            'classes': ('collapse',),
+        })
