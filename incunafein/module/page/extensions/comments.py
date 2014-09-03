344911d5555d07bbc7016900d91e119c560c79c0
Max Peterson <max@incuna.com>
2014-09-03 15:38:27 +0100

    Update comments extension.

--- incunafein/module/page/extensions/comments.py
+++ incunafein/module/page/extensions/comments.py
@@ -4,11 +4,19 @@ Add a allow_comments flag to the page
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
-def register(cls, admin_cls):
-    cls.add_to_class('allow_comments', models.BooleanField(_('allow comments'), default=False,))
-
-    admin_cls.list_filter += ['allow_comments',]
+from feincms import extensions
 
 
+class Extension(extensions.Extension):
+    def handle_model(self):
+        self.model.add_to_class(
+            'allow_comments',
+            models.BooleanField(_('allow comments'), default=False,),
+        )
 
+    def handle_modeladmin(self, modeladmin):
+        modeladmin.extend_list('list_filter', ['allow_comments'])
+        modeladmin.add_extension_options(_('Allow comments'), {
+            'fields': ('allow_comments',),
+            'classes': ('collapse',),
+        })
