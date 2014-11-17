93b9c2de13c6c6ff9eeb01573f2519752e53ea29
Matt Lenc <mattl@incuna.com>
2014-11-17 15:31:39 +0000

    Bump to v2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "incuna-feincms",
     packages = find_packages(),
     include_package_data=True,
-    version = '1.1.0',
+    version = '2.0.0',
     description = "Provides enhancements to FeinCMS.",
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
